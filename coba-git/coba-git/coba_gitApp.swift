6bc3dc75bb7bcfa8f38e3e243e68e4e40df29d2d
Henrian Pranata <henrianpranata@Henrians-Laptop.local>
2024-05-14 10:22:00 +0700

    membuat project xcode baru

---
+++ coba-git/coba-git/coba_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  coba_gitApp.swift
+//  coba-git
+//
+//  Created by Henrian Pranata on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct coba_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
